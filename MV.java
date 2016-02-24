6f6964743cdf5d85a737a6f8a4ed9f8c10c4d322
jonas001 <jonas.youtube@hotmail.de>
2016-02-24 16:53:19 +0100

    methoden

---
+++ MV.java
@@ -0,0 +1,80 @@
+
+public class MV
+{
+    private int passwort;
+    private String name;
+
+    public MV()
+    {
+        
+    }
+
+    public String getFreiMedien(String zeit)
+    {
+        return "";
+    }
+    
+    public String getFreieRäume(String zeit)
+    {
+        return "";
+    }
+    
+    public String getRaum()
+    {
+        return "";
+    }
+    
+    public String getMedien()
+    {
+        return "";
+    }
+    
+    public void setRaum(String zeit, int RaumID)
+    {
+    }
+    
+    public void removeRaum(String zeit, int RaumID)
+    {
+    }
+    
+    public void setMedium(String zeit, int mediumID)
+    {
+    }
+    
+    public void removeMedium(String zeit, int mediumID)
+    {
+    }
+    
+    public void login(String name, String passwort)
+    {
+    }
+    
+    public void logout()
+    {
+    }
+    
+    public void createRaum()
+    {
+    }
+    
+    public void createMedium()
+    {
+    }
+    
+    public void removeRaum()
+    {
+    }
+    
+    public void removeMedium()
+    {
+    }
+    
+    public String abfrage(String querry)
+    {
+        return "";
+    }
+    
+    public void setData(String wayne)
+    {
+    }
+}
